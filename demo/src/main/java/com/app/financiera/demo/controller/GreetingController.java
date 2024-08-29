fd15b281894178a89861e98705d56682ca8e5542
chava100f <chava100f@gmail.com>
2024-08-29 16:35:00 -0600

    greetings hi ;)

--- demo/src/main/java/com/app/financiera/demo/controller/GreetingController.java
+++ demo/src/main/java/com/app/financiera/demo/controller/GreetingController.java
@@ -1,4 +1,20 @@
 package com.app.financiera.demo.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("api/greetings")
 public class GreetingController {
+
+    @GetMapping("/hi")
+    public ResponseEntity<String> greetings(){
+
+        return new ResponseEntity<>("Hey you, Hi!", HttpStatus.OK);
+    }
 }
